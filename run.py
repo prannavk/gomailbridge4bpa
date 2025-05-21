f4fc7fb71802675474411eda6e193be57fcdca01
w3_kpranav_70088 <krishnapk808@gmail.com>
2025-05-21 19:52:15 +0530

    c6 small run.py update

--- run.py
+++ run.py
@@ -15,7 +15,8 @@ def home():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=FLASK_PORT)
+    app.run(debug=False, host="0.0.0.0", port=FLASK_PORT)
+    
 
 
 
