9fd46914c830aebd1fe85ceed11bd1ce095d7f4b
w3_kpranav_70088 <krishnapk808@gmail.com>
2025-05-23 15:31:38 +0530

    auth exp

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -7,7 +7,7 @@ API_KEY = os.getenv("API_KEY")
 def require_api_key(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        key = request.headers.get("x-api-key")
+        key = request.headers.get("lemma")
         if not key or key != API_KEY:
             return jsonify({"error": "Unauthorized"}), 401
         return func(*args, **kwargs)
